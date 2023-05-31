3caf30dcccccc6801586d868394a5c4982ddf23e
AbiHanna-Guy <AbiHanna.Guy@bcg.com>
2023-05-31 13:19:33 +0200

    added sudo

--- aws_deploy/setup_ec2.sh
+++ aws_deploy/setup_ec2.sh
@@ -5,4 +5,4 @@ sudo usermod -a -G docker ec2-user
 wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
 sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
 sudo chmod -v +x /usr/local/bin/docker-compose
-reboot
+sudo reboot
