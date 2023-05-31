35cd85970f9829d7ea1ebaaf9f689d9c7e70940c
AbiHanna-Guy <AbiHanna.Guy@bcg.com>
2023-05-31 13:17:21 +0200

    updates

---
+++ aws_deploy/setup_ec2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum install git
+sudo yum install docker
+sudo usermod -a -G docker ec2-user
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
