5ef85c3fc2d7c8552bb4bec4fff3dbcfaa83fcb3
AbiHanna-Guy <AbiHanna.Guy@bcg.com>
2023-05-31 13:18:38 +0200

    added reboot

--- aws_deploy/setup_ec2.sh
+++ aws_deploy/setup_ec2.sh
@@ -5,3 +5,4 @@ sudo usermod -a -G docker ec2-user
 wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
 sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
 sudo chmod -v +x /usr/local/bin/docker-compose
+reboot
