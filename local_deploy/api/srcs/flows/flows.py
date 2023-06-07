bf806e595b91ba8ef1bd994dcf392d5677511cc0
AbiHanna-Guy <AbiHanna.Guy@bcg.com>
2023-06-07 16:33:03 +0200

    bug fixes

--- local_deploy/api/srcs/flows/flows.py
+++ local_deploy/api/srcs/flows/flows.py
@@ -3,7 +3,8 @@ from srcs.tasks.celery import ManagedTask, worker
 import time
 
 @worker.task(base=ManagedTask)
-async def flow_placeholder(placeholder_id: int) -> None:
+async def flow_placeholder(placeholder_id: int) -> Placeholder:
     placeholder: Placeholder = await Placeholder.get(id=placeholder_id)
     time.sleep(2)
     print(f"Placeholder: {placeholder}")
+    return placeholder
